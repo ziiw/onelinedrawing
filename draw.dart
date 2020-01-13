1cc6889b25c5f87cfb1fe4ec8138dcfda328f48c
Janin Grandne <janin.grandne@gmail.com>
2020-01-13 11:53:12 +0100

    upload of login & draw

---
+++ draw.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+//Done with the Help of M T E C H V I R A L tutorial
+
+void main() => runApp(new MaterialApp(
+  home: new HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Offset> _points = <Offset>[];
+  //Initialize the points
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Container(
+        child: new GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            //here when starts drawing
+            setState(() {
+             RenderBox object = context.findRenderObject(); 
+             Offset _localPosition = 
+             object.globalToLocal(details.globalPosition);
+             _points = new List.from(_points)..add(_localPosition);
+
+            });
+
+
+          },
+
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: new CustomPaint(
+            painter: new Signature(points: _points), 
+            size: Size.infinite),
+          //Need to add up here the boolean to stop after the first line drawn
+        ),
+    ),
+    floatingActionButton: new FloatingActionButton
+    (child: new Icon(Icons.clear),
+    onPressed: () => _points.clear(),
+    ),
+    );
+  }
+}
+
+class Signature extends CustomPainter{
+  List<Offset> points;
+
+  Signature({this.points});
+
+  @override 
+  void paint(Canvas canvas, Size size) {
+    
+    Paint paint = new Paint()
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 5.0;
+
+    for (int i = 0; i < points.length - 1; i++){
+      if (points[i] != null && points[i + 1] != null){
+        canvas.drawLine(points[i], points[i + 1], paint);
+      }
+    }
+
+  }
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) => oldDelegate.points != points;
+}
